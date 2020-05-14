92e9f54c7a5253e96edce348a649ae9c95de866c
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-05-14 14:54:06 +0200

    Oprettet Autocamper metoder

--- src/main/java/com/example/demo/Service/AutocamperService.java
+++ src/main/java/com/example/demo/Service/AutocamperService.java
@@ -1,7 +1,30 @@
 package com.example.demo.Service;
 
+import com.example.demo.Model.Autocamper;
+import com.example.demo.Repository.AutocamperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutocamperService {
+    @Autowired
+    AutocamperRepository ar;
+
+    public List<Autocamper> listAutocampere(){
+        return ar.listAutocampere();
+    }
+
+    public Autocamper findAutocamperMedId(int id){
+        return ar.findAutocamperMedId(id);
+    }
+
+    public Autocamper tilfojAutocamper(Autocamper autocamper){
+        return ar.tilfojAutocamper(autocamper);
+    }
+
+    public Boolean sletAutocamper(int id){
+        return ar.sletAutocamper(id);
+    }
 }
