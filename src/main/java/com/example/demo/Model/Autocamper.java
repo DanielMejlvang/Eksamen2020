fc446e1ac716335e783624a949b4eb9de69cb916
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-05-14 15:07:55 +0200

    Fixet Autocamper metoder

--- src/main/java/com/example/demo/Model/Autocamper.java
+++ src/main/java/com/example/demo/Model/Autocamper.java
@@ -7,26 +7,26 @@ import javax.persistence.Id;
 public class Autocamper {
     @Id
     private int a_id;
-    private String type;
-    private String maerke;
-    private String model;
-    private int odometer;
-    private int senge;
-    private String nummerplade;
-    private int pris;
+    private String a_type;
+    private String a_maerke;
+    private String a_model;
+    private int a_odometer;
+    private int a_senge;
+    private String a_nummerplade;
+    private int a_pris;
 
     public Autocamper() {
     }
 
-    public Autocamper(int a_id, String type, String maerke, String model, int odometer, int senge, String nummerplade, int pris) {
+    public Autocamper(int a_id, String a_type, String a_maerke, String model, int odometer, int senge, String nummerplade, int pris) {
         this.a_id = a_id;
-        this.type = type;
-        this.maerke = maerke;
-        this.model = model;
-        this.odometer = odometer;
-        this.senge = senge;
-        this.nummerplade = nummerplade;
-        this.pris = pris;
+        this.a_type = a_type;
+        this.a_maerke = a_maerke;
+        this.a_model = model;
+        this.a_odometer = odometer;
+        this.a_senge = senge;
+        this.a_nummerplade = nummerplade;
+        this.a_pris = pris;
     }
 
     public int getA_id() {
@@ -37,59 +37,59 @@ public class Autocamper {
         this.a_id = a_id;
     }
 
-    public String getType() {
-        return type;
+    public String getA_type() {
+        return a_type;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setA_type(String type) {
+        this.a_type = type;
     }
 
-    public String getMaerke() {
-        return maerke;
+    public String getA_maerke() {
+        return a_maerke;
     }
 
-    public void setMaerke(String maerke) {
-        this.maerke = maerke;
+    public void setA_maerke(String maerke) {
+        this.a_maerke = maerke;
     }
 
-    public String getModel() {
-        return model;
+    public String getA_model() {
+        return a_model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
+    public void setA_model(String model) {
+        this.a_model = model;
     }
 
-    public int getOdometer() {
-        return odometer;
+    public int getA_odometer() {
+        return a_odometer;
     }
 
-    public void setOdometer(int odometer) {
-        this.odometer = odometer;
+    public void setA_odometer(int odometer) {
+        this.a_odometer = odometer;
     }
 
-    public int getSenge() {
-        return senge;
+    public int getA_senge() {
+        return a_senge;
     }
 
-    public void setSenge(int senge) {
-        this.senge = senge;
+    public void setA_senge(int senge) {
+        this.a_senge = senge;
     }
 
-    public String getNummerplade() {
-        return nummerplade;
+    public String getA_nummerplade() {
+        return a_nummerplade;
     }
 
-    public void setNummerplade(String nummerplade) {
-        this.nummerplade = nummerplade;
+    public void setA_nummerplade(String nummerplade) {
+        this.a_nummerplade = nummerplade;
     }
 
-    public int getPris() {
-        return pris;
+    public int getA_pris() {
+        return a_pris;
     }
 
-    public void setPris(int pris) {
-        this.pris = pris;
+    public void setA_pris(int pris) {
+        this.a_pris = pris;
     }
 }
